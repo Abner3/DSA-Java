2b5212e486d850a16fd273e2d4a778144d7b2248
Abner <Abner3@users.noreply.github.com>
2025-05-12 14:05:59 -0700

    Added ThreeNumberSum.java

--- src/ThreeNumberSum.java
+++ src/ThreeNumberSum.java
@@ -5,12 +5,20 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class ThreeNumberSum {
 
 
     public static void main(String[] args) {
+        List<Integer> randomNumbers = Arrays.asList(23, 45, 67, 12, 89, 34, 56, 78, 90, 11);
+        System.out.println(randomNumbers);
         System.out.println(1);
+        // Iterate through the list of numbers and print each to the console
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            System.out.println(randomNumbers.get(i));
+        }
+
     }
 
 }
